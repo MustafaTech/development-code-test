dfd030bdf0b203e21c9c786d4b15c480c59014eb
mustafa <mustafa@mustafapc>
2019-10-19 07:39:21 +0100

    .

--- src/main/java/uk/co/huntersix/spring/rest/referencedata/PersonDataService.java
+++ src/main/java/uk/co/huntersix/spring/rest/referencedata/PersonDataService.java
@@ -18,27 +18,26 @@ public class PersonDataService {
 																	new Person("Brian", "Archer"), 
 																	new Person("Collin", "Brown")
 																);
-	private final List<Person> allPeople = new ArrayList<>(PERSON_DATA);
-
+	
 	public Person findPerson(String lastName, String firstName) {
-		return allPeople.stream()
+		return PERSON_DATA.stream()
 				.filter(p -> p.getFirstName().equalsIgnoreCase(firstName) && p.getLastName().equalsIgnoreCase(lastName))
 				.findFirst()
 				.orElseThrow(PersonNotFoundException::new);
 	}
 
 	public void addPerson(Person person) {
-		boolean doesPersonExits = allPeople.stream()
+		boolean doesPersonExits = PERSON_DATA.stream()
 				.anyMatch(p -> p.getFirstName().equalsIgnoreCase(person.getFirstName())
 						&& p.getLastName().equalsIgnoreCase(person.getLastName()));
 		if (doesPersonExits) {
 			throw new PersonAlreadyExistsException();
 		}
-		allPeople.add(person);
+		PERSON_DATA.add(person);
 	}
 
 	public List<Person> findByLastName(String lastName) {
-		return allPeople.stream()
+		return PERSON_DATA.stream()
 				.filter(p -> p.getLastName().equalsIgnoreCase(lastName))
 				.collect(Collectors.toList());
 	}
